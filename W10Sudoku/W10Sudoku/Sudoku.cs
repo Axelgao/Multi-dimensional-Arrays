72fa758a29e6453ffb695336944c91f62f7004dc
Axelgao <gaoshenchuan@hotmail.com>
2017-01-10 14:15:13 +1300

    Add files via upload

---
+++ W10Sudoku/W10Sudoku/Sudoku.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W10Sudoku
+{
+    public class Sudoku
+    {
+        public bool VerifyAll(int[,] sudokuBoard, int size)
+        {
+            bool result=true;
+            for(int i = 0; i < size; i++)
+            {
+                if (result == true)
+                {
+                    result = VerifyRow(sudokuBoard, i, size);
+                }
+                else break;
+                if (result == true)
+                {
+                    result = VerifyColumn(sudokuBoard, i, size);
+                }
+                else break;
+            }
+            return result;
+        }
+        public bool VerifyColumn(int[,] sudokuBoard, int column, int size)
+        {
+            bool[] matchedNumbers = new bool[size];
+            int numberOfBools = 0;
+            //1.create an array of bools
+            for (int i = 0; i < size; ++i) matchedNumbers[i] = false;
+            //2.toggle the bools to trues
+            matchedNumbers = ToggleArrayOfBoolsByColumn(sudokuBoard, column, size, matchedNumbers);
+            //3.count the trues in array
+            for (int i = 0; i < size; ++i)
+            {
+                if (matchedNumbers[i] == true) numberOfBools++;
+            }
+            //4.return if trues equal size of board
+            return numberOfBools == size;
+        }
+        private bool[] ToggleArrayOfBoolsByColumn(int[,] sudokuBoard, int column, int size, bool[] matchedNumbers)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                int num = sudokuBoard[i, column];
+                if (num > 0 && num <= size) matchedNumbers[num - 1] = true;
+            }
+            return matchedNumbers;
+        }
+        public bool VerifyRow(int[,] sudokuBoard, int row, int size)
+        {
+            bool[] matchedNumbers = new bool[size];
+            int numberOfBools = 0;
+            //1.create an array of bools
+            for (int i = 0 ; i < size; ++i) matchedNumbers[i] = false;
+            //2.toggle the bools to trues
+            matchedNumbers = ToggleArrayOfBools(sudokuBoard, row, size, matchedNumbers);
+            //3.count the trues in array
+            for (int i = 0; i < size; ++i)
+            {
+                if (matchedNumbers[i] == true) numberOfBools++;
+            }
+            //4.return if trues equal size of board
+            return numberOfBools == size;
+        }
+        private bool[] ToggleArrayOfBools(int[,] sudokuBoard, int row, int size, bool[] matchedNumbers)
+        {
+            for (int i=0; i < size; i++)
+            {
+                int num = sudokuBoard[row, i];
+                if (num > 0 && num <= size) matchedNumbers[num - 1] = true;
+            }
+            return matchedNumbers;
+        }
+
+    }
+}
---
+++ W10Sudoku/W10SudokuTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using W10Sudoku;
+
+namespace W10SudokuTest
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void VerifyRowIsOkTest()
+        {
+            int[,] sudokuBoard = { {1,2,3,4}, {1,3,4,2} };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsTrue(sudoku.VerifyRow(sudokuBoard, 1, 4));
+        }
+
+        [TestMethod()]
+        public void VerifyColumnIsOkTest()
+        {
+            int[,] sudokuBoard = { { 1, 2, 3, 4 }, { 2, 3, 4, 2 }, { 3, 2, 3, 4 }, { 4, 2, 3, 4 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsTrue(sudoku.VerifyColumn(sudokuBoard, 0, 4));
+        }
+
+        [TestMethod()]
+        public void VerifyColumnIsWrongTest()
+        {
+            int[,] sudokuBoard = { { 1, 2, 3, 4 }, { 2, 3, 4, 2 }, { -1, 2, 3, 4 }, { 4, 2, 3, 4 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsFalse(sudoku.VerifyColumn(sudokuBoard, 0, 4));
+        }
+
+        [TestMethod()]
+        public void VerifyColumnIsTooLargeTest()
+        {
+            int[,] sudokuBoard = { { 1, 2, 3, 4 }, { 2, 3, 4, 2 }, { 5, 2, 3, 4 }, { 4, 2, 3, 4 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsFalse(sudoku.VerifyColumn(sudokuBoard, 0, 4));
+        }
+
+        [TestMethod()]
+        public void VerifyAllTest()
+        {
+            int[,] sudokuBoard = { { 2, 1, 4, 3 }, { 4, 3, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsTrue(sudoku.VerifyAll(sudokuBoard, 4));
+        }
+        [TestMethod()]
+        public void VerifyAllTestBreak()
+        {
+            int[,] sudokuBoard = { { 3, 1, 4, 3 }, { 4, 3, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsTrue(sudoku.VerifyAll(sudokuBoard, 4));
+        }
+        [TestMethod()]
+        public void VerifyRowIsWrongTest()
+        {
+            int[,] sudokuBoard = { { 3, 1, 4, 3 }, { 4, 3, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsFalse(sudoku.VerifyRow(sudokuBoard, 0, 4));
+        }
+
+        [TestMethod()]
+        public void VerifyRowIsTooLargeTest()
+        {
+            int[,] sudokuBoard = { { 3, 1, 4, 3 }, { 4, 3, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };
+            Sudoku sudoku = new Sudoku();
+            Assert.IsFalse(sudoku.VerifyRow(sudokuBoard, 0, 4));
+        }
+    }
+}
